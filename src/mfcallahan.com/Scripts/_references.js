06d715b80c8696ab5239114ba3a26aa0a59f2fda
Matthew Callahan <matthew.callahan@outlook.com>
2018-04-08 13:14:06 -0700

    deleted app

--- src/mfcallahan.com/Scripts/_references.js
+++
@@ -1,7 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="site.js" />
-/// <reference path="bootstrap.js" />
-/// <reference path="jquery.validate.min.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="jquery-3.2.1.js" />
-/// <reference path="modernizr-2.8.3.js" />
