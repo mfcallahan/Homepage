b072fb4d950746c030c6b14efa91220eb56ce0f7
Matthew Callahan <matthew.callahan@outlook.com>
2018-06-08 21:13:55 -0700

    moving API out of mfcallahan.com

--- src/mfcallahan.com/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Homepage.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- src/mfcallahan.com/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Homepage.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
