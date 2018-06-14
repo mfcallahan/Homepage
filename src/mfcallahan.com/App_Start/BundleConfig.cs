381e618b8784f371037cac4bd6f471cf034cacb8
Matthew Callahan <matthew.callahan@outlook.com>
2018-06-14 16:18:44 -0700

    _Layout cleanup

--- src/mfcallahan.com/App_Start/BundleConfig.cs
+++ src/mfcallahan.com/App_Start/BundleConfig.cs
@@ -18,7 +18,7 @@ namespace Homepage
 
             // site scripts
             bundles.Add(new ScriptBundle("~/bundles/scripts-site").Include(
-                "~/Scripts/jquery-3.2.1.min.js",
+                "~/Scripts/jquery-3.3.1.min.js",
                 "~/Scripts/bootstrap.min.js",
                 "~/Scripts/featherlight.min.js",
                 "~/Scripts/featherlight.gallery.min.js",
