18a0e0047fca1f19beeaef89635bb49d79485917
Matthew Callahan <matthew.callahan@outlook.com>
2018-06-15 14:13:29 -0700

    web.config cleanup

--- src/mfcallahan.com/Controllers/HomeController.cs
+++ src/mfcallahan.com/Controllers/HomeController.cs
@@ -4,19 +4,16 @@ namespace Homepage.Controllers
 {
     public class HomeController : Controller
     {
-        // mfcallahan.com 
         public ActionResult Index()
         {
             return View();
         }
 
-        // mfcallahan.com/radio 
         public ActionResult Radio()
         {
             return View();
         }
-
-        // mfcallahan.com/blog 
+        
         public ActionResult Blog()
         {
             return Redirect("https://seesharpdotnet.wordpress.com/");
