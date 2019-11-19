1135c6046326797b74beae8bfee34452c7435c55
Matthew Callahan <matthew.callahan@outlook.com>
2019-11-19 12:31:04 -0700

    add profile pic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProfilePicComponent } from './profile-pic/profile-pic.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, ProfilePicComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/profile-pic/profile-pic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-pic',
+  templateUrl: './profile-pic.component.html',
+  styleUrls: ['./profile-pic.component.scss']
+})
+export class ProfilePicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
