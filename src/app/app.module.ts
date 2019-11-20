3259e2113dec470dbdaafbfda6d8d25c0fefbd0d
Matthew Callahan <matthew.callahan@outlook.com>
2019-11-20 11:28:02 -0700

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProfilePicComponent } from './profile-pic/profile-pic.component';
 import { ContactLinksComponent } from './contact-links/contact-links.component';
+import { EducationCertLinksComponent } from './education-cert-links/education-cert-links.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ProfilePicComponent, ContactLinksComponent],
+  declarations: [AppComponent, HomeComponent, ProfilePicComponent, ContactLinksComponent, EducationCertLinksComponent, AboutComponent],
   imports: [BrowserModule, FontAwesomeModule, NgbModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
