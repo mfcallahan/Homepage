33e70bdf895a1bc06c814e3b0c4be8e64703dcd0
Matt Callahan <matthew.callahan@outlook.com>
2020-06-19 11:09:16 -0700

    update to angular 9

--- src/app/hammerGestureConfig.ts
+++ src/app/hammerGestureConfig.ts
@@ -2,7 +2,9 @@
 // https://github.com/lukasz-galka/ngx-gallery/issues/242#issuecomment-517998231
 
 import { HammerGestureConfig } from '@angular/platform-browser';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class CustomHammerConfig extends HammerGestureConfig {
   overrides = {
     pinch: { enable: false },
