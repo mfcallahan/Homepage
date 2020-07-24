9581941e07ebc384d5e1bbf47c7c8eb6263444c1
Matt Callahan <matthew.callahan@outlook.com>
2020-07-24 15:21:11 -0700

    update fonts and layout

--- src/app/homepage/homepage-main/contact-links/contact-links.component.ts
+++ src/app/homepage/homepage-main/contact-links/contact-links.component.ts
@@ -5,7 +5,7 @@ import { faGithubSquare, faWordpressSimple, faLinkedin } from '@fortawesome/free
 @Component({
   selector: 'app-homepage-contact-links',
   templateUrl: './contact-links.component.html',
-  styleUrls: ['../../contact-links/contact-links.scss'],
+  styleUrls: ['../../contact-links/contact-links.component.scss'],
 })
 export class ContactLinksComponent {
   faEnvelope = faEnvelope;
