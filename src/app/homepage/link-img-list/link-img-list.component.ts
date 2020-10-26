a89b31d1db3280e86f192949b17d2df0af4abd5f
Matt Callahan <matthew.callahan@outlook.com>
2020-10-26 14:31:52 -0700

    add tetriz to projects, update dev tools list

--- src/app/homepage/link-img-list/link-img-list.component.ts
+++ src/app/homepage/link-img-list/link-img-list.component.ts
@@ -3,9 +3,13 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-link-img-list',
   templateUrl: './link-img-list.component.html',
-  styleUrls: ['./link-img-list.component.scss']
+  styleUrls: ['./link-img-list.component.scss'],
 })
 export class LinkImgListComponent {
+  @Input()
+  leftColSize: number;
+  @Input()
+  rightColSize: number;
   @Input()
   title: string;
   @Input()
