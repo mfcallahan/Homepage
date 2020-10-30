e217a5f03e32f21353f41cd504e7ddbf9b370011
Matt Callahan <matthew.callahan@outlook.com>
2020-10-30 15:44:24 -0700

    adding some tests

---
+++ src/app/urlRedirector.spec.ts
@@ -0,0 +1,21 @@
+import { ActivatedRouteSnapshot } from '@angular/router';
+import { UrlRedirector } from './urlRedirector';
+
+describe('UrlRedirector', () => {
+  xit('should set window.location.href to external url and return true', () => {
+    // Arrange
+    // TODO: mock window object
+    const activatedRouteSnapshot = new ActivatedRouteSnapshot();
+    activatedRouteSnapshot.data = { route: 'foo/bar' };
+
+    const urlRedirector = new UrlRedirector();
+
+    // Act
+    // const result = urlRedirector.canActivate(activatedRouteSnapshot);
+
+    // Assert
+    // expect(window.location.href).toBe(activatedRouteSnapshot.data.route);
+    // expect(result).toBe(true);
+    expect(1).toBe(1);
+  });
+});
