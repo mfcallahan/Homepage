c48faab7f34c91a996570eec03604a3f04141cb4
Matt Callahan <matthew.callahan@outlook.com>
2020-10-30 09:58:32 -0700

    add UrlRedirector to blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,53 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageMainComponent } from 'src/app/homepage/homepage-main/homepage-main.component';
 import { HomepageRadioComponent } from 'src/app/homepage/homepage-radio/homepage-radio.component';
+import { UrlRedirector } from 'src/app/urlRediretor';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomepageMainComponent
+    component: HomepageMainComponent,
   },
   {
     path: 'radio',
-    component: HomepageRadioComponent
-  }
+    component: HomepageRadioComponent,
+  },
+  {
+    path: 'resume',
+    canActivate: [UrlRedirector],
+    component: UrlRedirector,
+    data: {
+      externalUrl: '/assets/docs/Matthew_Callahan_resume.pdf',
+    },
+  },
+  {
+    path: 'blog',
+    canActivate: [UrlRedirector],
+    component: UrlRedirector,
+    data: {
+      externalUrl: 'https://seesharpdotnet.wordpress.com/',
+    },
+  },
+  {
+    path: 'github',
+    canActivate: [UrlRedirector],
+    component: UrlRedirector,
+    data: {
+      externalUrl: 'https://github.com/mfcallahan/',
+    },
+  },
+  {
+    path: 'linkedin',
+    canActivate: [UrlRedirector],
+    component: UrlRedirector,
+    data: {
+      externalUrl: 'https://www.linkedin.com/in/mfcallahan1/',
+    },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
