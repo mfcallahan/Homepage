0a8d072d8f44c563bd38b9077ba942557e35ae99
Matt Callahan <matthew.callahan@outlook.com>
2020-11-09 10:02:14 -0700

    add types

--- src/app/homepage/homepage-radio/bandplan/bandplan.component.ts
+++ src/app/homepage/homepage-radio/bandplan/bandplan.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faBroadcastTower } from '@fortawesome/free-solid-svg-icons';
+import { BandplanTableRow } from 'src/app/models/bandplanTableRow';
 
 @Component({
   selector: 'app-homepage-radio-bandplan',
@@ -8,160 +9,48 @@ import { faBroadcastTower } from '@fortawesome/free-solid-svg-icons';
 })
 export class BandplanComponent implements OnInit {
   faBroadcastTower = faBroadcastTower;
-  tableRows: Array<any>;
+  tableRows: BandplanTableRow[];
 
   constructor() {}
 
   ngOnInit() {
-    this.setBandPlan();
+    this.tableRows = this.getBandPlan();
   }
 
-  private setBandPlan(): void {
-    this.tableRows = [
-      {
-        channel: '1',
-        name: 'Ch 1 Simplex',
-        frequency: '462.5625 MHz',
-        tone: '',
-        bandwidth: 'Narrow',
-        power: 'Low',
-        notes: 'FRS 1/Natl Simplex',
-      },
-      {
-        channel: '2',
-        name: 'Ch 2 Simplex',
-        frequency: '462.5875 MHz',
-        tone: '',
-        bandwidth: 'Narrow',
-        power: 'Low',
-        notes: 'FRS 2',
-      },
-      {
-        channel: '3',
-        name: 'Ch 3 Simplex',
-        frequency: '462.6125 MHz',
-        tone: '',
-        bandwidth: 'Narrow',
-        power: 'Low',
-        notes: 'FRS 3',
-      },
-      {
-        channel: '4',
-        name: 'Ch 4 Simplex',
-        frequency: '462.5375 MHz',
-        tone: '',
-        bandwidth: 'Narrow',
-        power: 'Low',
-        notes: 'FRS 4',
-      },
-      {
-        channel: '5',
-        name: 'Ch 5 Simplex',
-        frequency: '462.6675 MHz',
-        tone: '',
-        bandwidth: 'Narrow',
-        power: 'Low',
-        notes: 'FRS 5',
-      },
-      {
-        channel: '6',
-        name: 'Ch 6 Simplex',
-        frequency: '462.6875 MHz',
-        tone: '',
-        bandwidth: 'Narrow',
-        power: 'Low',
-        notes: 'FRS 6',
-      },
-      {
-        channel: '7',
-        name: 'Ch 7 Simplex',
-        frequency: '462.7125 MHz',
-        tone: '',
-        bandwidth: 'Narrow',
-        power: 'Low',
-        notes: 'FRS 7',
-      },
-      {
-        channel: '8',
-        name: 'Ch 8 Simplex',
-        frequency: '462.5500 MHz',
-        tone: '141.3 Hz',
-        bandwidth: 'Narrow',
-        power: 'High',
-        notes: 'GMRS 15/PHX 550 T',
-      },
-      {
-        channel: '9',
-        name: 'Ch 9 Simplex',
-        frequency: '462.5750 MHz',
-        tone: '69.3 Hz',
-        bandwidth: 'Narrow',
-        power: 'High',
-        notes: 'GMRS 16',
-      },
-      {
-        channel: '10',
-        name: 'Ch 10 Simplex',
-        frequency: '462.6000 MHz',
-        tone: '69.3 Hz',
-        bandwidth: 'Narrow',
-        power: 'High',
-        notes: 'GMRS 17',
-      },
-      {
-        channel: '11',
-        name: 'Ch 11 Simplex',
-        frequency: '462.6250 MHz',
-        tone: '69.3 Hz',
-        bandwidth: 'Narrow',
-        power: 'High',
-        notes: 'GMRS 18',
-      },
-      {
-        channel: '12',
-        name: 'Ch 12 Simplex',
-        frequency: '462.6500 MHz',
-        tone: '69.3 Hz',
-        bandwidth: 'Narrow',
-        power: 'High',
-        notes: 'GMRS 19',
-      },
-      {
-        channel: '13',
-        name: 'Ch 13 Simplex',
-        frequency: '462.6750 MHz',
-        tone: '141.3 Hz',
-        bandwidth: 'Narrow',
-        power: 'High',
-        notes: 'GMRS 20/Natl Simplex',
-      },
-      {
-        channel: '14',
-        name: 'Ch 14 Simplex',
-        frequency: '462.7000 MHz',
-        tone: '141.3 Hz',
-        bandwidth: 'Narrow',
-        power: 'High',
-        notes: 'GMRS 21/Camping Alpha',
-      },
-      {
-        channel: '15',
-        name: 'Ch 15 Simplex',
-        frequency: '462.7250 MHz',
-        tone: '69.3 Hz',
-        bandwidth: 'Narrow',
-        power: 'High',
-        notes: 'GMRS 22/Camping Bravo',
-      },
-      {
-        channel: '16',
-        name: 'Ch 16 Simplex',
-        frequency: '462.5750 MHz',
-        tone: '',
-        bandwidth: 'Narrow',
-        power: 'High',
-        notes: 'GMRS 16/4x4 Off-Road Channel',
-      },
+  private getBandPlan(): BandplanTableRow[] {
+    return [
+      new BandplanTableRow('1', 'Ch 1 Simplex', '462.5625 MHz', '', 'Narrow', 'Low', 'FRS 1/Natl Simplex'),
+      new BandplanTableRow('2', 'Ch 2 Simplex', '462.5875 MHz', '', 'Narrow', 'Low', 'FRS 2'),
+      new BandplanTableRow('3', 'Ch 3 Simplex', '462.6125 MHz', '', 'Narrow', 'Low', 'FRS 3'),
+      new BandplanTableRow('4', 'Ch 4 Simplex', '462.5375 MHz', '', 'Narrow', 'Low', 'FRS 4'),
+      new BandplanTableRow('5', 'Ch 5 Simplex', '462.6675 MHz', '', 'Narrow', 'Low', 'FRS 5'),
+      new BandplanTableRow('6', 'Ch 6 Simplex', '462.6875 MHz', '', 'Narrow', 'Low', 'FRS 6'),
+      new BandplanTableRow('7', 'Ch 7 Simplex', '462.7125 MHz', '', 'Narrow', 'Low', 'FRS 7'),
+      new BandplanTableRow('8', 'Ch 8 Simplex', '462.5500 MHz', '141.3 Hz', 'Narrow', 'High', 'GMRS 15/PHX 550 T'),
+      new BandplanTableRow('9', 'Ch 9 Simplex', '462.5750 MHz', '69.3 Hz', 'Narrow', 'High', 'GMRS 16'),
+      new BandplanTableRow('10', 'Ch 10 Simplex', '462.6000 MHz', '69.3 Hz', 'Narrow', 'High', 'GMRS 17'),
+      new BandplanTableRow('11', 'Ch 11 Simplex', '462.6250 MHz', '69.3 Hz', 'Narrow', 'High', 'GMRS 18'),
+      new BandplanTableRow('12', 'Ch 12 Simplex', '462.6500 MHz', '69.3 Hz', 'Narrow', 'High', 'GMRS 19'),
+      new BandplanTableRow('13', 'Ch 13 Simplex', '462.6750 MHz', '141.3 Hz', 'Narrow', 'High', 'GMRS 20/Natl Simplex'),
+      new BandplanTableRow(
+        '14',
+        'Ch 14 Simplex',
+        '462.7000 MHz',
+        '141.3 Hz',
+        'Narrow',
+        'High',
+        'GMRS 21/Camping Alpha'
+      ),
+      new BandplanTableRow('15', 'Ch 15 Simplex', '462.7250 MHz', '69.3 Hz', 'Narrow', 'High', 'GMRS 22/Camping Bravo'),
+      new BandplanTableRow(
+        '16',
+        'Ch 16 Simplex',
+        '462.5750 MHz',
+        '69.3 Hz',
+        'Narrow',
+        'High',
+        'GMRS 16/4x4 Off-Road Channel'
+      ),
     ];
   }
 }
