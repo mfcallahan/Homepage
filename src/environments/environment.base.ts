3a00faa7ce09e5a9d113ab57868967b11ba56ca6
Matt Callahan <matthew.callahan@outlook.com>
2021-04-22 16:34:25 -0700

    remove comments

--- src/environments/environment.base.ts
+++ src/environments/environment.base.ts
@@ -1,6 +1,5 @@
 export const baseConfigs = {
   wordPressApi: {
-    // posts: 'https://public-api.wordpress.com/rest/v1.1/sites/mfcallahan.blog/posts',
     posts: 'https://public-api.wordpress.com/rest/v1.1/sites/seesharpdotnet.wordpress.com/posts',
   },
 };
