87f972919087a216a7142d4a10d25c2f83c9d88e
Matt Callahan <matthew.callahan@outlook.com>
2021-08-17 07:24:45 -0700

    prettier all the things

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -10,22 +10,22 @@ module.exports = function(config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/homepage'),
       reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
     },
     customLaunchers: {
       ChromeTestDebugging: {
         base: 'Chrome',
-        flags: [ '--no-sandbox', '--remote-debugging-port=9333'],
-        debug: true
-      }
+        flags: ['--no-sandbox', '--remote-debugging-port=9333'],
+        debug: true,
+      },
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -34,6 +34,6 @@ module.exports = function(config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
   });
 };
--- src/app/homepage/profile-pic/profile-pic.component.ts
+++ src/app/homepage/profile-pic/profile-pic.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-profile-pic',
   templateUrl: './profile-pic.component.html',
-  styleUrls: ['./profile-pic.component.scss']
+  styleUrls: ['./profile-pic.component.scss'],
 })
 export class ProfilePicComponent {
   @Input()
